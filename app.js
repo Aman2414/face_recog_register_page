2278280599eb49b1d3d91515b9402e3053f9893a
alishawadhwaniya <aw377541@gmail.com>
2022-08-14 18:54:36 +0530

    Deploying

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const connection = require("./database/db");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
 const routes = require("./controllers/route");
+const port =  process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -11,6 +11,6 @@ app.use(express.static('register'));
 app.use(express.static('login'));
 app.use("/", routes);
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("Server Started");
 });
