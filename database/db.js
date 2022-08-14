110b6606088280b22e7e4c8d6716f902e2e05f9e
alishawadhwaniya <aw377541@gmail.com>
2022-08-14 18:37:50 +0530

    Ok

--- database/db.js
+++ database/db.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/face_recog", { useNewUrlParser: true }, (err) => {
-    if (!err) {
-        console.log("Database Connected");
-    }
-    else {
-        console.log("Error Connecting to database");
-    }
+mongoose.connect("mongodb+srv://aman:aman@cluster0.yiokapd.mongodb.net/face_recog?retryWrites=true&w=majority", { useNewUrlParser: true }).then(() => {
+    console.log("Connected To Database in Atlas");
+}).catch((e) => {
+    console.log("Error");
 });
+
 
 const User = require("../model/user_model.js");
