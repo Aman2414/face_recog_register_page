2f6f4ba45bf24b6d59b441282e7552a2679326a2
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-04 15:57:57 +0530

    Hello

--- controllers/route.js
+++ controllers/route.js
@@ -4,91 +4,98 @@ const mongoose = require("mongoose");
 const path = require("path");
 const router = express.Router();
 const UserModel = mongoose.model("users");
-const conn = require('../database/db');
-const fetch = require('node-fetch');
-const axios = require('axios');
+const conn = require("../database/db");
+const fetch = require("node-fetch");
+const axios = require("axios");
 
 router.get("/", (req, res) => {
-    res.send("Welcome to the Server");
+  res.send("Welcome to the Server");
 });
 
 router.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname.replace('controllers', "/register/register.html")));
-})
+  res.sendFile(
+    path.join(__dirname.replace("controllers", "/register/register.html"))
+  );
+});
 
 router.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname.replace('controllers', "/login/login.html")))
+  res.sendFile(
+    path.join(__dirname.replace("controllers", "/login/login.html"))
+  );
+});
+
+router.get("/voting", (req, res) => {
+  res.sendFile(
+    path.join(__dirname.replace("controllers", "/voting/voting.html"))
+  );
 });
 
 router.get("/getImageLink/:username", (req, res) => {
-    UserModel.findOne({ username: req.params.username }, (err, docs) => {
-        if (err) {
-            res.send("Error getting Image Link");
-        }
-        else {
-            res.send(docs["image_url"]);
-        }
-    });
+  UserModel.findOne({ username: req.params.username }, (err, docs) => {
+    if (err) {
+      res.send("Error getting Image Link");
+    } else {
+      res.send(docs["image_url"]);
+    }
+  });
 });
 
 router.post("/verifyUser", (req, res) => {
-    UserModel.findOne({ username: req.body.username, password: req.body.password }, (err, docs) => {
-        if (err) {
-            console.log("Error");
+  UserModel.findOne(
+    { username: req.body.username, password: req.body.password },
+    (err, docs) => {
+      if (err) {
+        console.log("Error");
+      } else {
+        if (docs != null) {
+          let image = docs["image_url"];
+          let testImage = req.body.img;
+          console.log("Calling Authenticate");
+
+          const data = {
+            image: image,
+            testImage: testImage,
+          };
+
+          //change this to heroku address
+          axios
+            .post(
+              "https://face-recog-python-api.herokuapp.com/authenticate",
+              data
+            )
+            .then((response) => {
+              console.log(response.data);
+              if (response.data["result"] == true) {
+                res.redirect("/voting");
+              } else {
+                res.send("Face Did not Match");
+              }
+            })
+            .catch((err) => {
+              res.send("Error" + err);
+            });
+        } else {
+          res.send("Invalid Username Or Password");
         }
-        else {
-            if (docs != null) {
-                let image = docs["image_url"];
-                let testImage = req.body.img;
-                console.log("Calling Authenticate");
-
-                const data = {
-                    "image": image,
-                    "testImage": testImage,
-                };
-
-                //change this to heroku address
-                axios.post("https://face-recog-python-api.herokuapp.com/authenticate", data).then((response) => {
-                    console.log(response.data);
-                    if (response.data['result'] == true)
-                    {
-                        res.redirect("/welcomePage");
-                    }
-                    else{
-                        res.send("Face Did not Match");
-                    }
-
-                }).catch((err) => {
-                    res.send("Error" + err);
-                });
-            }
-            else {
-                res.send("Invalid Username Or Password");
-            }
-        }
-    });
+      }
+    }
+  );
 });
-
-router.get("/welcomePage", (req, res) => {
-    res.send("Login Successfull");
-});
-
 
 router.post("/registerUser", (req, res) => {
-    console.log("Body ", req.body);
+  console.log("Body ", req.body);
 
-    UserModel.create(
-        {
-            username: req.body.username,
-            password: req.body.password,
-            image_url: req.body.imgData,
-        }).then(() => {
-            res.redirect("/login");
-        }).catch((err) => {
-            res.send("Unable to create User", err);
-        });
-
+  UserModel.create({
+    username: req.body.username,
+    password: req.body.password,
+    image_url: req.body.imgData,
+  })
+    .then(() => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      res.send("Unable to create User", err);
+    });
 });
-
 
 module.exports = router;
